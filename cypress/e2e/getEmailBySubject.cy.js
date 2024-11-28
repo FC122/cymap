00fa05f69a93c464ec954cff03fad4467fead20c
schmeb0704 <seanauditor0704@gmail.com>
2024-11-28 16:48:38 +0800

    test: add new tests for getSubjectByEmail

--- cypress/e2e/getEmailBySubject.cy.js
+++ cypress/e2e/getEmailBySubject.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 import emails from "../support/emails"
+import emails2 from "../support/emails2"
 
 describe('tests getEmailBySubject function', () => {
   before("Set configs and add mail", () => {
@@ -15,9 +16,9 @@ describe('tests getEmailBySubject function', () => {
         cy.sendMail(config, emails.helloWorld(i, i))
         cy.wait(2000)
       }
+      cy.sendMail(config, emails2.oneMoreTest())
     }).then(() => {
       cy.getEmailBySubject(`Hello World-${2}`).then(email => {
-        console.log("email", email)
         expect(email.to.text).to.contain(2)
         expect(email).to.have.property('attachments').that.is.an('array');
         expect(email).to.have.property('headers').that.is.an('object');
@@ -35,6 +36,27 @@ describe('tests getEmailBySubject function', () => {
         expect(email.from.value[0]).to.have.property('name').that.is.a('string').and.equals("");
         expect(email).to.have.property('messageId').that.is.a('string')
       })
+    })
+  })
+
+  it('should be able to get the email with the subject "One more test!!!"', () => {
+    cy.getEmailBySubject('One more test!!!').then((email) => {
+      expect(email.to.text).to.contain('moreTest')
+      expect(email).to.have.property('attachments').that.is.an('array');
+      expect(email).to.have.property('headers').that.is.an('object');
+      expect(email).to.have.property('headerLines').that.is.an('array').with.lengthOf.at.least(1);
+      expect(email).to.have.property('html').that.is.a('string').and.equals(emails2.oneMoreTest().html);
+      expect(email).to.have.property('text').that.is.a('string').and.equals(emails2.oneMoreTest().text);
+      expect(email).to.have.property('textAsHtml').that.is.a('string')
+      expect(email).to.have.property('subject').that.is.a('string').and.equals(emails2.oneMoreTest().subject);
+      expect(email).to.have.property('date').that.is.a('string')
+      expect(email).to.have.property('to').that.is.an('object').and.has.property('value').that.is.an('array').with.lengthOf(1);
+      expect(email.to.value[0]).to.have.property('address').that.is.a('string').and.equals(emails2.oneMoreTest().to);
+      expect(email.to.value[0]).to.have.property('name').that.is.a('string').and.equals("");
+      expect(email).to.have.property('from').that.is.an('object').and.has.property('value').that.is.an('array').with.lengthOf(1);
+      expect(email.from.value[0]).to.have.property('address').that.is.a('string').and.equals(emails2.oneMoreTest().from);
+      expect(email.from.value[0]).to.have.property('name').that.is.a('string').and.equals("");
+      expect(email).to.have.property('messageId').that.is.a('string')
     })
   })
 
